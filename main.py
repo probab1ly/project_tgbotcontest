82dc0b7f3bac1a783a4339b66dd26344288365cf
Дмитрий <142750962+probab1ly@users.noreply.github.com>
2025-06-17 22:39:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from database import init_db
 async def main():
     logging.basicConfig(level=logging.INFO)
     await init_db()
-    bot = Bot(token='7755632572:AAGYBbI-LTMSZ1a45mWD_IEDT1uqTsQuCwg')
+    bot = Bot(token='')
     dp = Dispatcher(storage=SQLiteStorage(path='states.db'))
     dp.include_router(router)
     await dp.start_polling(bot)
