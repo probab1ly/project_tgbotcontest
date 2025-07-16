7d457be37a1e1a02a023afcd24bf1ef5889ad6ea
Дмитрий <142750962+probab1ly@users.noreply.github.com>
2025-07-16 01:08:44 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import asyncio
 import logging
 from aiogram import Bot, Dispatcher
-from sqlitestorage.storage import SQLiteStorage
+# from sqlitestorage.storage import SQLiteStorage
 from handlers import router
 from database import init_db
 async def main():
     logging.basicConfig(level=logging.INFO)
     await init_db()
     bot = Bot(token='')
-    dp = Dispatcher(storage=SQLiteStorage(path='states.db'))
+    dp = Dispatcher() #storage=SQLiteStorage(path='states.db')
     dp.include_router(router)
     await dp.start_polling(bot)
 if __name__ == '__main__':
